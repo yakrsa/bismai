03253adc0411d7ab580fc7f87e47d636127ecabf
yakrsa <yakrsa@qq.com>
2014-06-07 00:26:15 +0800

    ignore

--- Data/conf/info.php
+++ Data/conf/info.php
@@ -1,9 +1,9 @@
 <?php 
 return array (
-  'site_name' => '艾米微管家',
-  'site_title' => '中国最大的微信、易信公众服务平台',
-  'site_url' => 'http://127.0.0.1',
-  'site_my' => '艾米',
+  'site_name' => '比斯迈科技',
+  'site_title' => '微信公众服务平台',
+  'site_url' => 'http://bismai.com',
+  'site_my' => '比斯迈',
   'ischeckuser' => '1',
   'user_first_grade' => '4',
   'user_first_grade_time' => '30',
@@ -18,5 +18,5 @@ return array (
   'keyword' => '微信、易信帮手 微信、易信公众账号 微信、易信公众平台 微信、易信公众账号开发 微信、易信二次开发 微信、易信接口开发 微信、易信托管服务 微信、易信营销 微信、易信公众平台接口开发',
   'content' => '微信、易信公众平台接口开发、托管、营销活动、二次开发',
   'counts' => '',
-  'copyright' => '由艾米微管家提供技术支持',
+  'copyright' => '由bismai提供技术支持',
 );
