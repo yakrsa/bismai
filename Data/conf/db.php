1f7f68a68a5db946a865313202eb7d51b35c0376
yaks <yakrsa@qq.com>
2014-06-08 21:16:27 +0800

    0608 21:00 office

--- Data/conf/db.php
+++ Data/conf/db.php
@@ -2,10 +2,10 @@
 return array (
   'DB_TYPE' => 'mysql',
   'DB_HOST' => 'localhost',
-  'DB_PORT' => '3306',
+  'DB_PORT' => '2019',
   'DB_NAME' => 'wgc',
   'DB_USER' => 'root',
-  'DB_PWD' => 'Empire@qq',
+  'DB_PWD' => '',
   'DB_PREFIX' => 'wgc_',
 );
 ?>
