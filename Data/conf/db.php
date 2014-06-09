dbe36c0a0bea85c65b9b1f56a964d759fa1cda99
yaks <yakrsa@qq.com>
2014-06-09 09:29:44 +0800

    loading.gif 404

--- Data/conf/db.php
+++ Data/conf/db.php
@@ -2,10 +2,10 @@
 return array (
   'DB_TYPE' => 'mysql',
   'DB_HOST' => 'localhost',
-  'DB_PORT' => '2019',
+  'DB_PORT' => '3306',
   'DB_NAME' => 'wgc',
   'DB_USER' => 'root',
-  'DB_PWD' => '',
+  'DB_PWD' => 'Empire@qq',
   'DB_PREFIX' => 'wgc_',
 );
 ?>
