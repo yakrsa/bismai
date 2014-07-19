890c401119cf259b4961b783338758270098e8ea
yakrsa <yakrsa@qq.com>
2014-07-19 13:33:38 +0800

    authcode 1

--- Data/conf/db.php
+++ Data/conf/db.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'DB_TYPE' => 'mysql',
-  'DB_HOST' => 'rdsqyn6f2qyn6f2.mysql.rds.aliyuncs.com',
+  'DB_HOST' => 'zhaomingyuinter.mysql.rds.aliyuncs.com',
   'DB_PORT' => '3306',
   'DB_NAME' => 'bismai',
   'DB_USER' => 'bismai',
